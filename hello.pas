b8903c25b1b53c5c122b7c69e937831ee24cec33
local user <user@mil8a-118b-03.mmcs.sfedu.ru>
2023-09-30 10:23:57 +0300

    About time of the day

--- hello.pas
+++ hello.pas
@@ -1,1 +1,9 @@
-﻿Print('hello world!');
+﻿begin
+  var n := ReadInteger('введите время суток:');
+  case n of
+    4..10: Println('Доброе утро, мир!');
+    11..16: Println('Добрый вечер, мир!');
+    17..22: Println('Доброй ночи, мир!');
+    else Println('Доброй ночи, мир!');
+  end;
+end.
