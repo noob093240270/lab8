6a6bbcdddcd16368325665ec8d667289d23b050e
local user <user@mil8a-118b-03.mmcs.sfedu.ru>
2023-09-30 10:31:57 +0300

    add file time.pas

---
+++ time.pas
@@ -0,0 +1,12 @@
+﻿begin
+  var n := ReadInteger('введите год: ');
+  if (n mod 4 = 0) then
+  begin
+    if (n mod 100 = 0) and (n mod 400 <> 0) then
+      Print('год не вискосный')
+    else
+      Print('год високосный')
+  end
+  else
+    Print('год не високосный');
+end.
